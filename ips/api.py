6f4896be9d78cc8f997e6ea069154ceedece7f59
Damien Saucez <damien.saucez@gmail.com>
2024-11-08 23:22:38 +0100

    .

--- ips/api.py
+++ ips/api.py
@@ -606,6 +606,7 @@ from typing import Dict, Any
 
 @app.post("/ns")
 async def post_ns(body: Dict[str, Any]):
-    print (body)
-    print (body["request"]["userInfo"]["username"])
+    user = body["request"]["userInfo"]["username"]
+    namespace = body["request"]["namespace"]
+    print (f"{user} : {namespace}")
     return {"body": body}
