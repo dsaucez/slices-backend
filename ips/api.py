b6c10e71e4690059b4ce52ccbf62cfbd2f490e0a
Damien Saucez <damien.saucez@gmail.com>
2024-11-09 00:23:26 +0100

    debug

--- ips/api.py
+++ ips/api.py
@@ -116,6 +116,9 @@ def check_role(allowed_roles: List[str]):
     return role_checker
 
 def validate_token(request: Request, token: Optional[str] = Security(api_key_header)):
+
+    print (token)
+
     decoded = jwt.decode(token, options={'verify_signature': False})
     # TBD check that it is correct!!!
     
