31ac9fe47b23e424cefc4ea732289a5f94e62d2c
Damien Saucez <damien.saucez@gmail.com>
2024-11-13 11:34:10 +0100

    logger

--- ips/api.py
+++ ips/api.py
@@ -127,7 +127,8 @@ def validate_token(token: str = Security(api_key_header)):
                     detail="Token has expired",
                     headers={"WWW-Authenticate": "Bearer"},
                 )
-    logging.INFO(dict(decoded)["preferred_username"])
+    logger = logging.getLogger(__name__)
+    logger.INFO(dict(decoded)["preferred_username"])
     return decoded
 
 # def validate_token(request: Request, token: str = Security(api_key_header)):
