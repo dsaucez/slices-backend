ae06f61b734efd95c28b19c887fb6686143a6cce
Damien Saucez <damien.saucez@gmail.com>
2024-11-26 15:58:56 +0100

    fail

--- ips/api.py
+++ ips/api.py
@@ -246,6 +246,7 @@ async def add_process_after_request(request: Request, call_next):
     return response
 
 async def process_after_request():
+    dsf
     print("save db after each API call")
     save_db()
 
